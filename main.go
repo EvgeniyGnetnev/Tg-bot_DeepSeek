7266f1ce0c6af89700886f285db5a8a5cb1a0094
EvgeniyGnetnev <chaklittle@yandex.ru>
2025-01-30 00:39:20 +0300

    Добавил клиент для тг бота

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"os"
+
+	"github.com/EvgeniyGnetnev/Tg-bot_DeepSeek/clients/telegram"
+)
 
 func main() {
-
+	tgClient := telegram.New(os.Getenv("TG_BOT_HOST"), os.Getenv("TG_BOT_TOKEN") )
 }
