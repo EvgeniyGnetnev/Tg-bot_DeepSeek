f4f38938efe6384c9ca77af7af328ef150685e02
EvgeniyGnetnev <chaklittle@yandex.ru>
2025-01-30 13:25:11 +0300

    Добавил storage в файловой системе

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"os"
+	// "os"
 
-	"github.com/EvgeniyGnetnev/Tg-bot_DeepSeek/clients/telegram"
+	// "github.com/EvgeniyGnetnev/Tg-bot_DeepSeek/clients/telegram"
 )
 
 func main() {
-	tgClient := telegram.New(os.Getenv("TG_BOT_HOST"), os.Getenv("TG_BOT_TOKEN") )
+	// tgClient := telegram.New(os.Getenv("TG_BOT_HOST"), os.Getenv("TG_BOT_TOKEN") )
 }
